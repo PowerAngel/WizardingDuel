8b3edec77615bdfe5cb2813c25acc6e38778ab2d
Jonatan <Jonatan.Berg@live.com>
2019-07-24 20:01:39 +0200

    Added scene for Non-VR. Wand movement not done.

---
+++ Assets/Scripts/WandMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = targetPos;
+        }
+    }
+}
