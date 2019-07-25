9e9b8de400fcd79af7be313b031d85e80bc2ebb6
Jonatan <Jonatan.Berg@live.com>
2019-07-25 14:07:07 +0200

    Added Debug Board for the non-VR project.

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "Welcome to Wizarding Duel!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
