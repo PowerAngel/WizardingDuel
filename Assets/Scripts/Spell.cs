0d680aaa3fb1cafe28a07d4461ff1113ce0d8a1d
Jonatan <Jonatan.Berg@live.com>
2019-07-30 15:23:12 +0200

    Added Snape as a moving target. Added five different spells with different specs that gets chosen randomly when casting spell.

---
+++ Assets/Scripts/Spell.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell
+{
+    private string name;
+    private int power;
+    private int damage;
+    private Material material;
+
+    public Spell(string name, int power, int damage, Material material)
+    {
+        this.name = name;
+        this.power = power;
+        this.damage = damage;
+        this.material = material;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+
+    public int getPower()
+    {
+        return power;
+    }
+
+    public int getDamage()
+    {
+        return damage;
+    }
+
+    public Material getMaterial()
+    {
+        return material;
+    }
+
+}
---
+++ Assets/Scripts/TargetMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TargetMovement : MonoBehaviour
+{
+
+    public double moveTime = 2;
+    public int speed = 1;
+
+    DateTime _lastEventTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lastEventTime = DateTime.UtcNow;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((DateTime.Now - _lastEventTime) < TimeSpan.FromSeconds(moveTime))
+        {
+            transform.position += transform.right * Time.deltaTime;
+        }
+        else if((DateTime.Now - _lastEventTime) < TimeSpan.FromSeconds(moveTime*2))
+        {
+            transform.position -= transform.right * Time.deltaTime;
+        }
+        else
+        {
+            _lastEventTime = DateTime.Now;
+        }
+    }
+}
