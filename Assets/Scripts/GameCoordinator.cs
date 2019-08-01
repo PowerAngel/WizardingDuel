85bf0fd506cc51a22c9eb3f29c4fadbdd2cb150a
Jonatan <Jonatan.Berg@live.com>
2019-08-01 14:20:03 +0200

    Now plots two different trajectories on startup.

--- Assets/Scripts/GameCoordinator.cs
+++ Assets/Scripts/GameCoordinator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameCoordinator : MonoBehaviour
 {
     public GameObject myPrefab;
+    public GameObject spellParticlePlot;
     //private GameObject[] Targets;
     List<GameObject> Targets;
     // Start is called before the first frame update
@@ -17,6 +18,30 @@ public class GameCoordinator : MonoBehaviour
         tempObject.transform.rotation = Quaternion.LookRotation(direction);
 
         Targets.Add(tempObject);
+
+        Trajectory trajectoryA = new Trajectory("data/wand_SmallHeart.txt");
+        List<Aff3d> posesA_ = trajectoryA.GetTrajectory();
+
+        foreach(Aff3d pose in posesA_)
+        {
+            GameObject gameObject = Instantiate(spellParticlePlot);
+            gameObject.transform.position = new Vector3(pose.Translation.x, pose.Translation.y, pose.Translation.z);
+            Vector3 rotation = pose.Rotation.eulerAngles;
+            gameObject.transform.rotation = Quaternion.Euler(rotation);
+        }
+
+        Trajectory trajectoryB = new Trajectory("data/wand_LargeHeart.txt");
+        List<Aff3d> posesB_ = trajectoryB.GetTrajectory();
+
+        foreach(Aff3d pose in posesB_)
+        {
+            GameObject gameObject = Instantiate(spellParticlePlot);
+            gameObject.transform.position = new Vector3(pose.Translation.x, pose.Translation.y, pose.Translation.z);
+            Vector3 rotation = pose.Rotation.eulerAngles;
+            gameObject.transform.rotation = Quaternion.Euler(rotation);
+        }
+
+
     }
 
     // Update is called once per frame
