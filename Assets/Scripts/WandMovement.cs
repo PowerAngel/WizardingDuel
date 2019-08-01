d2f5e23fb5ddac7e94f39b615bef5c4f068a05e7
Björn <friman.1@hotmail.com>
2019-08-01 21:09:01 +0200

    no VR i gitignore

--- Assets/Scripts/WandMovement.cs
+++ Assets/Scripts/WandMovement.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WandMovement : MonoBehaviour
 {
+    public Text text;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +15,12 @@ public class WandMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            var targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = targetPos;
+            Debug.Log("pressed left click");
+            //var targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //transform.position = targetPos;
+            //text.text = "Mousepos: " + targetPos.ToString();
         }
     }
 }
